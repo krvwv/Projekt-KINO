18c5b9862c687aa5445fd58f8f502ddf31932888
krvwv <pkarwo@wp.pl>
2020-01-26 15:01:48 +0100

    Update Program.cs

--- Projekt KINO/Program.cs
+++ Projekt KINO/Program.cs
@@ -13,28 +13,7 @@ namespace Projekt_KINO
     {
         static void Main(string[] args)
         {
-            #region
-            /*//Kino
-            Kino CinemaCity = new Kino("CinemaCity", "Kraków");
-
-            //Sala
-            Sala sala1 = new Sala("1", 10, 2, 5);
-            Sala sala2 = new Sala("2", 20, 4, 5);
-            CinemaCity.DodajSale(sala1);
-            CinemaCity.DodajSale(sala2);
-            sala2.Miejsca();
-            //Film
-            Film spiderman = new Film("Spiderman", "Sam Raimi", 120, 16, Film.narracja.dubbing, Film.gatunki.akcja,czy_3D.nie);
-            Film thor = new Film("Thor", "Kenneth Branagh", 114, 12, Film.narracja.dubbing, Film.gatunki.akcja,czy_3D.nie);
-            CinemaCity.DodajFilm(spiderman);
-            CinemaCity.DodajFilm(thor);
-            //Console.WriteLine(CinemaCity);
-            //Seans
-            Seans seans1 = new Seans(new DateTime(2020, 1, 20, 10, 30, 0), sala1, spiderman); //dodac sporo seansow
-            Seans seans2 = new Seans(new DateTime(2020, 1, 20, 10, 30, 0), sala2, thor);
-            CinemaCity.DodajSeans(seans1);
-            CinemaCity.DodajSeans(seans2);*/
-            #endregion
+           
             Kino CinemaCity = Funkcje.InizjalizacjaKina();
             Menu menu = new Menu();
             Widz w = new Widz();
